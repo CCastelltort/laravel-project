92c165e9780683717ec628ec2332d1a4c6c43d6c
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-06-05 12:05:12 +0200

    Allowed updating and deleting jobs

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index. Display all jobs
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
    
@@ -15,18 +16,20 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//Display form to create a job
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 
 });
 
-
+//Show single job
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job.show', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 
 });
 
+//Stores a new job
 Route::post('/jobs', function () {
     //Validation
     request()->validate([
@@ -43,6 +46,52 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+//Display Edit job form
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+
+});
+
+//Update job
+Route::patch('/jobs/{id}', function ($id) {
+    //Validation
+    request()->validate([
+        'title'=> ['required', 'min:3'],
+        'salary'=> ['required']
+    ]);
+
+    //Authorize
+
+    //Update job
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title'=> request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    //Redirect to the job page
+    return redirect('/jobs/'. $job->id);
+
+});
+
+//Delete job
+Route::delete('/jobs/{id}', function ($id) {
+    //Authorize request
+
+    //Delete job
+    Job::findOrFail($id)->delete();
+
+    //Redirect
+    return redirect('/jobs');
+});
+
+
+//Display contact page
 Route::get('/contact', function () {
     return view('contact');
 });
+
+
+
