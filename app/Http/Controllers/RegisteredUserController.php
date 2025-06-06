5b53642ea031100e6470f6d8bd3af0d8484b5c6f
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-06-06 10:19:04 +0200

    finished registration and log in

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+
 
 class RegisteredUserController extends Controller
 {
@@ -11,6 +15,20 @@ class RegisteredUserController extends Controller
     }
 
     public function store() {
-        return view('auth.register');
+        //Validate
+        $validatedAttributes = request()->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        //Create the user
+        $user = User::create($validatedAttributes);
+
+        //Log in
+        Auth::login($user);
+
+        //Redirect somewhere
+        return redirect('/jobs');
     }
 }
